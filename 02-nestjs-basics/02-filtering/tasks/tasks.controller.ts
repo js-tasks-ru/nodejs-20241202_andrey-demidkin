026d656a3c459a97f76864d253342095cc0ff2a6
Andrey Demidkin <creator13rus@gmail.com>
2024-12-12 14:21:48 +0300

    add solution for 2-module 2-task

--- 02-nestjs-basics/02-filtering/tasks/tasks.controller.ts
+++ 02-nestjs-basics/02-filtering/tasks/tasks.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Get, Query } from "@nestjs/common";
+import {Controller, Get, HttpException, HttpStatus, Query} from "@nestjs/common";
 import { TasksService } from "./tasks.service";
-import { TaskStatus } from "./task.model";
+import {TASK_KEYS, TaskKeys, TaskStatus} from "./task.model";
 
 @Controller("tasks")
 export class TasksController {
@@ -11,5 +11,29 @@ export class TasksController {
     @Query("status") status?: TaskStatus,
     @Query("page") page?: number,
     @Query("limit") limit?: number,
-  ) {}
+    @Query("sortBy") sortBy?: TaskKeys,
+  ) {
+    const wrongSortParameter = sortBy && !TASK_KEYS.includes(sortBy);
+    const wrongPageParameter = page && typeof Number(page) !== "number"
+      || page && page < 1
+    const wrongLimitParameter = limit && typeof Number(limit) !== "number"
+      || limit && limit < 1
+    const wrongStatusParameter = status && !Object.values(TaskStatus).includes(status);
+
+    if(wrongLimitParameter) {
+      throw new HttpException(`Wrong limit parameter`, HttpStatus.BAD_REQUEST);
+    }
+    if(wrongPageParameter ) {
+      throw new HttpException(`Wrong page parameter`, HttpStatus.BAD_REQUEST);
+    }
+    if(wrongSortParameter) {
+      throw new HttpException(`Wrong sortBy parameter`, HttpStatus.BAD_REQUEST);
+    }
+
+    if(wrongStatusParameter) {
+      throw new HttpException(`Wrong task status:${status}`, HttpStatus.NOT_FOUND);
+    }
+
+    return this.tasksService.getFilteredTasks(status, page, limit, sortBy);
+  }
 }
--- 02-nestjs-basics/02-filtering/tasks/tasks.service.ts
+++ 02-nestjs-basics/02-filtering/tasks/tasks.service.ts
@@ -1,5 +1,23 @@
-import { Injectable } from "@nestjs/common";
-import { Task, TaskStatus } from "./task.model";
+import {Injectable} from "@nestjs/common";
+import {Task, TaskKeys, TaskStatus} from "./task.model";
+import {chunk} from "lodash";
+
+//метод сортировки задач по полям
+const sortByTaskField = (sortBy: TaskKeys, tasks:Task[]) => tasks.sort((taskA, taskB)=> {
+  //если поле сортировки "id" то сравниваем как числа
+  let valueA: number | string = sortBy === 'id' ? Number(taskA[sortBy]) : taskA[sortBy];
+  let valueB: number | string = sortBy === 'id' ? Number(taskB[sortBy]) : taskB[sortBy];
+
+  if(valueA < valueB) {
+    return -1;
+  }
+
+  if(valueA > valueB) {
+    return 1;
+  }
+
+  return 0;
+})
 
 @Injectable()
 export class TasksService {
@@ -40,5 +58,39 @@ export class TasksService {
     status?: TaskStatus,
     page?: number,
     limit?: number,
-  ): Task[] {}
+    sortBy?: TaskKeys,
+  ): Task[] {
+    let result = this.tasks;
+    //сначала фильтруем задачи по статусу
+    if(status) {
+      result = result.filter(task => task.status === status);
+    }
+
+    //сортируем по полю, если нужно
+    if(sortBy) {
+      result = sortByTaskField(sortBy, result);
+    }
+
+    //если есть limit
+    if(limit) {
+      //разбиваем одномерный массив задач.
+      //получаем двумерный. Кол-во элементов во вложенном = limit
+      const tasksByPage = chunk(result, limit);
+
+      //если есть page и он больше 1,
+      // то обращаемся к массиву задач по индексу page - 1.
+      //если элемента с таким индексом нет, то возвращаем пустой массив
+      //если же page нет, то возвращаем первый элемент
+      return (page)
+        ? tasksByPage[page - 1] ?? []
+        : tasksByPage[0]
+    }
+
+    //если нет лимита и page больше 1,то возвращаем пустой массив
+    if(page && page > 1) {
+      return []
+    }
+
+    return result
+  }
 }
