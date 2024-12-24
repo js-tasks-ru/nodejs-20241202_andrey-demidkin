aa3709d2ce9afc8e4bf182c4ee9243badffb9311
Demidkin Andrey <creator13rus@gmail.com>
2024-12-24 22:34:18 +0300

    add solution for 3-module 1-task

--- 03-di/01-notification-service/providers/NotificationService.ts
+++ 03-di/01-notification-service/providers/NotificationService.ts
@@ -1,1 +1,15 @@
-export class NotificationService {}
+import {Injectable} from "@nestjs/common";
+
+@Injectable()
+export class NotificationService {
+
+  sendEmail(email:string, messageTheme: string, message: string) {
+    const logMessage = `Email sent to ${email}: [${messageTheme}] ${message}`;
+    console.log(logMessage)
+  }
+
+  sendSMS(phone: string, message:string) {
+    const logMessage = `SMS sent to ${phone}: ${message}`;
+    console.log(logMessage)
+  }
+}
